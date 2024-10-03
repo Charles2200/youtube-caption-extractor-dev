9b7c14bffa6daec9a5476810d141f1ee3c9c5bb8
Charles2200 <112788167+Charles2200@users.noreply.github.com>
2024-10-03 14:40:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getSubtitles, getVideoDetails } = require('youtube-caption-extractor');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Rota para buscar legendas
 app.get('/api/subtitles', async (req, res) => {
